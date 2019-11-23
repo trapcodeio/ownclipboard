e4c8aad9461015a7ea4b1c6069f2368b3865de01
hibrid <samuelnmeje@gmail.com>
2019-11-23 18:27:44 +0100

    added help page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <template>
   <div id="app">
     <router-view/>
+
+<div class="has-text-centered  has-text-centered">
+  <router-link class="navbar-item" to="/">Home</router-link>
+  <router-link class="navbar-item" to="/help">Help</router-link>
+  <router-link class="navbar-item" to="/docs">Api Documentation</router-link>
+</div>
+
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+
+  #app{
+    min-height: calc(100vh - 70px);
+    /*height: 100vh;*/
+
+
+
+
+  }
+
+  h1{
+    height: 50px;
+    /*position: relative;*/
+    /*bottom: 50px;*/
+  }
+
+
+
+
+
+</style>
