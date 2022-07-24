077020a44b4d8879a7ac49437face0c27746f3ae
trapcodeio <trapcode.io@gmail.com>
2022-07-24 15:50:07 +0100

    Updated server side packages

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,10 +1,10 @@
-"use strict";
-const bcrypt = require("bcryptjs");
+'use strict';
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const DataSchema = {
-    username: 3,
-    password: 3
+  username: 3,
+  password: 3,
 };
 
 /**
@@ -12,122 +12,127 @@ const DataSchema = {
  * @type {Xpresser.Controller.Handler}
  */
 const AuthController = $.handler({
-    // Controller Name
-    name: "AuthController",
-    // Controller Default Service Error Handler.
-    e: $$.defaultErrorHandler,
-
-    // Boot AuthController
-    boot: async (http) => {
-        let username = http.query().removeNull(true).get('username', false);
-
-        if (!username) {
-            username = http.body().removeNull(true).get('username', username)
-        }
-
-        if (username) username = username.toLowerCase();
-
-        const user = (typeof username === "string" && !(username.length < DataSchema.username)) ? await User.query().where({username}).first() : false;
-        return {user, username}
-    },
-
-    auth: (http) => {
-        const logged = http.isLogged();
-        const user = http.authUser();
-
-        if (user) {
-            user.$pick([
-                'username',
-            ])
-        }
-
-        return http.toApi({
-            user: logged ? user : false,
-            logged,
-        })
-    },
-
-    checkUsername: async (http, boot, error) => {
-        const {user, username} = boot;
-
-        if (username && username.length < DataSchema.username)
-            return error(`Username too short.`);
-
-        return http.toApi({exists: !!user});
-    },
-
-    login: async (http, boot, error) => {
-        const {user, username} = boot;
-        if (!user) return error(`Account with  ${username} does not exist.`);
-
-        let password = http.body().removeNull(true).get('password', '');
-        if (password.length < DataSchema.password) {
-            return error(`Password too short.`)
-        }
-
-        const isPassword = bcrypt.compareSync(password, user.password);
-        if (!isPassword) {
-            return error(`Password incorrect for username: ${username}`)
-        }
-
-        http.loginUser(username);
-
-        user.$pick(User.jsPick());
-
-        // Set Session Keys
-        return http.toApi({user, __say: `Login successful.`})
-    },
-
-    register: async (http, boot, error) => {
-        const {user, username} = boot;
-        if (user) return error(`Username ${username} already exists.`);
-
-        let password = http.body().removeNull(true).get('password', '');
-        if (password.length < 4) {
-            return error(`Password too short.`)
-        }
-
-        password = bcrypt.hashSync(password, 10);
-        const NewUser = {username, password};
-        // Add user to database
-        await User.query().insert(NewUser);
-
-        return http.sayToApi(`Registration successful, login now!`)
-    },
-
-    logout: (http) => {
-        http.logout();
-        return http.toApi({logged: http.isLogged()});
-    },
-
-    change_password: (http, boot, error) => {
-        const needed = ['current', 'password'];
-        const body = http.body();
-        const user = http.authUser();
-
-        if (!user) {
-            return error(`You are not logged!`);
-        }
-
-        if (!body.removeNull(true).exists(needed)) {
-            return error(`Fields missing.`);
-        }
-
-        const {current, password} = body.pick(needed);
-
-        if (!bcrypt.compareSync(current, user.password)) {
-            return error('Old password provided is not correct!')
-        }
-
-        user.$query().updateAndFetch({
-            password: bcrypt.hashSync(password, 10)
-        });
-
-        http.logout();
-
-        return http.sayToApi(`Password changed successfully, please re-login.`);
+  // Controller Name
+  name: 'AuthController',
+  // Controller Default Service Error Handler.
+  e: $$.defaultErrorHandler,
+  
+  // Boot AuthController
+  boot: async (http) => {
+    let username = http.$query.removeNullOrUndefined().get('username');
+    
+    if (!username) {
+      username = http.$body.removeNullOrUndefined().get('username', username);
     }
+    
+    if (username) username = username.toLowerCase();
+    
+    const user = (typeof username === 'string' &&
+        !(username.length < DataSchema.username)) ? await User.query().
+        where({username}).
+        first() : false;
+    return {user, username};
+  },
+  
+  auth: (http) => {
+    const logged = http.isLogged();
+    const user = http.authUser();
+    
+    if (user) {
+      user.$pick([
+        'username',
+      ]);
+    }
+    
+    return http.toApi({
+      user: logged ? user : false,
+      logged,
+    });
+  },
+  
+  checkUsername: async (http, boot, error) => {
+    const {user, username} = boot;
+    
+    console.log(boot);
+    
+    if (username && username.length < DataSchema.username)
+      return error(`Username too short.`);
+    
+    return http.toApi({exists: !!user});
+  },
+  
+  login: async (http, boot, error) => {
+    const {user, username} = boot;
+    if (!user) return error(`Account with  ${username} does not exist.`);
+    
+    let password = http.$body.removeNullOrUndefined().get('password', '');
+    
+    if (password.length < DataSchema.password) {
+      return error(`Password too short.`);
+    }
+    
+    const isPassword = bcrypt.compareSync(password, user.password);
+    if (!isPassword) {
+      return error(`Password incorrect for username: ${username}`);
+    }
+    
+    http.loginUser(username);
+    
+    user.$pick(User.jsPick());
+    
+    // Set Session Keys
+    return http.toApi({user, __say: `Login successful.`});
+  },
+  
+  register: async (http, boot, error) => {
+    const {user, username} = boot;
+    if (user) return error(`Username ${username} already exists.`);
+    
+    let password = http.body().removeNull(true).get('password', '');
+    if (password.length < 4) {
+      return error(`Password too short.`);
+    }
+    
+    password = bcrypt.hashSync(password, 10);
+    const NewUser = {username, password};
+    // Add user to database
+    await User.query().insert(NewUser);
+    
+    return http.sayToApi(`Registration successful, login now!`);
+  },
+  
+  logout: (http) => {
+    http.logout();
+    return http.toApi({logged: http.isLogged()});
+  },
+  
+  change_password: (http, boot, error) => {
+    const needed = ['current', 'password'];
+    const body = http.body();
+    const user = http.authUser();
+    
+    if (!user) {
+      return error(`You are not logged!`);
+    }
+    
+    if (!body.removeNull(true).exists(needed)) {
+      return error(`Fields missing.`);
+    }
+    
+    const {current, password} = body.pick(needed);
+    
+    if (!bcrypt.compareSync(current, user.password)) {
+      return error('Old password provided is not correct!');
+    }
+    
+    user.$query().updateAndFetch({
+      password: bcrypt.hashSync(password, 10),
+    });
+    
+    http.logout();
+    
+    return http.sayToApi(`Password changed successfully, please re-login.`);
+  },
 });
-
 
 module.exports = AuthController;
