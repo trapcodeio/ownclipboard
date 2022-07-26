e4e4f5db72092921e639d910f1cd6fd8e2f3ab0b
trapcodeio <trapcode.io@gmail.com>
2022-07-26 13:11:23 +0100

    add search

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,41 +1,35 @@
-'use strict';
-const BaseModel = require('./BaseModel');
+"use strict";
+const BaseModel = require("./BaseModel");
 
 /**
  * User Model
  
  */
 class User extends BaseModel {
-  
-  /**
-   * Name of model database table name.
-   * @method tableName
-   * @returns {string}
-   */
-  static get tableName() {
-    return 'users';
-  }
-  
-  /**
-   * @return {Promise<*>}
-   * @param value
-   */
-  static async authDataProvider(value) {
-    return User.query().where({username: value}).first();
-  }
-  
-  /**
-   * Pick data returned to frontend javascript
-   * @returns []
-   */
-  static jsPick() {
-    return [
-      'email',
-      'username',
-      'last_seen',
-    ];
-  }
-  
+    /**
+     * Name of model database table name.
+     * @method tableName
+     * @returns {string}
+     */
+    static get tableName() {
+        return "users";
+    }
+
+    /**
+     * @return {Promise<*>}
+     * @param value
+     */
+    static async authDataProvider(value) {
+        return User.query().where({ username: value }).first();
+    }
+
+    /**
+     * Pick data returned to frontend javascript
+     * @returns []
+     */
+    static jsPick() {
+        return ["email", "username", "last_seen"];
+    }
 }
 
 User.prototype.$hidden = [];
